4e0f58209332e7ed8f874d217a40f89309791317
jerome Morissard <morissardj@gmail.com>
2013-12-07 22:34:01 +0100

    init

---
+++ DesignPatterns/DesignPatterns/JMOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JMOAppDelegate.h
+//  DesignPatterns
+//
+//  Created by Jerome Morissard on 12/7/13.
+//  Copyright (c) 2013 Jerome Morissard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DesignPatterns/DesignPatterns/JMOViewController.m
@@ -0,0 +1,29 @@
+//
+//  JMOViewController.m
+//  DesignPatterns
+//
+//  Created by Jerome Morissard on 12/7/13.
+//  Copyright (c) 2013 Jerome Morissard. All rights reserved.
+//
+
+#import "JMOViewController.h"
+
+@interface JMOViewController ()
+
+@end
+
+@implementation JMOViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
